4215c4f4e5e4d36cbfc7165094e21c34ae910799
Johannes Elferich <jojotux123@hotmail.com>
2022-11-16 19:34:08 +0000

    v0.1.5

--- tests/generate_runprofile.py
+++ tests/generate_runprofile.py
@@ -25,10 +25,72 @@ rp = RunProfile()
 rp.name = "96GPUs"
 rp.manager_command = manager_command
 rp.RemoveAll()
-
 for host in hosts:
     for igpu in range(0,8):
         rp.AddCommand(f'ssh -f {host} "unset CUDA_VISIBLE_DEVICES && export CUDA_VISIBLE_DEVICES={igpu} && {program_command}"',1,num_threads,False,0,delay)
-
 rpm.AddProfile(rp)
-rpm.WriteRunProfilesToDisk("/tmp/rr.txt",[0])
+
+rp = RunProfile()
+rp.name = "88GPUs"
+rp.manager_command = manager_command
+rp.RemoveAll()
+for host in hosts[1:]:
+    for igpu in range(0,8):
+        rp.AddCommand(f'ssh -f {host} "unset CUDA_VISIBLE_DEVICES && export CUDA_VISIBLE_DEVICES={igpu} && {program_command}"',1,num_threads,False,0,delay)
+rpm.AddProfile(rp)
+
+rp = RunProfile()
+rp.name = "48GPUs (1)"
+rp.manager_command = manager_command
+rp.RemoveAll()
+for host in hosts[:6]:
+    for igpu in range(0,8):
+        rp.AddCommand(f'ssh -f {host} "unset CUDA_VISIBLE_DEVICES && export CUDA_VISIBLE_DEVICES={igpu} && {program_command}"',1,num_threads,False,0,delay)
+rpm.AddProfile(rp)
+
+rp = RunProfile()
+rp.name = "40GPUs (1)"
+rp.manager_command = manager_command
+rp.RemoveAll()
+for host in hosts[1:6]:
+    for igpu in range(0,8):
+        rp.AddCommand(f'ssh -f {host} "unset CUDA_VISIBLE_DEVICES && export CUDA_VISIBLE_DEVICES={igpu} && {program_command}"',1,num_threads,False,0,delay)
+rpm.AddProfile(rp)
+
+rp = RunProfile()
+rp.name = "48GPUs (2)"
+rp.manager_command = manager_command
+rp.RemoveAll()
+for host in hosts[6:]:
+    for igpu in range(0,8):
+        rp.AddCommand(f'ssh -f {host} "unset CUDA_VISIBLE_DEVICES && export CUDA_VISIBLE_DEVICES={igpu} && {program_command}"',1,num_threads,False,0,delay)
+rpm.AddProfile(rp)
+
+rp = RunProfile()
+rp.name = "32GPUs (1)"
+rp.manager_command = manager_command
+rp.RemoveAll()
+for host in hosts[:4]:
+    for igpu in range(0,8):
+        rp.AddCommand(f'ssh -f {host} "unset CUDA_VISIBLE_DEVICES && export CUDA_VISIBLE_DEVICES={igpu} && {program_command}"',1,num_threads,False,0,delay)
+rpm.AddProfile(rp)
+
+rp = RunProfile()
+rp.name = "32GPUs (2)"
+rp.manager_command = manager_command
+rp.RemoveAll()
+for host in hosts[4:8]:
+    for igpu in range(0,8):
+        rp.AddCommand(f'ssh -f {host} "unset CUDA_VISIBLE_DEVICES && export CUDA_VISIBLE_DEVICES={igpu} && {program_command}"',1,num_threads,False,0,delay)
+rpm.AddProfile(rp)
+
+rp = RunProfile()
+rp.name = "32GPUs (3)"
+rp.manager_command = manager_command
+rp.RemoveAll()
+for host in hosts[8:]:
+    for igpu in range(0,8):
+        rp.AddCommand(f'ssh -f {host} "unset CUDA_VISIBLE_DEVICES && export CUDA_VISIBLE_DEVICES={igpu} && {program_command}"',1,num_threads,False,0,delay)
+rpm.AddProfile(rp)
+
+rpm.WriteRunProfilesToDisk("/tmp/rr.txt",[0,1,2,3,4,5,6,7])
